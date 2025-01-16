7d17df304127e9daab2be75ec345723c2f9ace03
Javed Khan (Node Dev) <javed.khan@technosoftwares.com>
2025-01-17 01:46:44 +0530

    sss

--- Jenkinsfile
+++ Jenkinsfile
@@ -21,7 +21,7 @@ pipeline{
         
         stage('Deploy'){
             steps {
-                bat 'pm2 startOrRestart pm2.config.json'
+                sh "npm start"
             }
         }
     }
--- server.js
+++ server.js
@@ -5,34 +5,33 @@ const app = express()
 // create application/json parser
 var bodyParser = require('body-parser')
 var jsonParser = bodyParser.json()
- 
+
 // create application/x-www-form-urlencoded parser
 //var urlencodedParser = bodyParser.urlencoded({ extended: false })
-const port = 3000
-const auth = (req,res,  next) => {
+const port = 8000
+const auth = (req, res, next) => {
 	const headers = req.headers;
-	if (headers['x-dsn']!='abcd-abcd')
-	{
+	if (headers['x-dsn'] != 'abcd-abcd') {
 		res.send('Auth Failed');
 		return;
 	}
 	next();
 }
-app.get('/',  (req, res) => {
+app.get('/', (req, res) => {
 	res.send('Hello World!');
-  })
-app.get('/testgetrequest',  (req, res) => {
-	res.send('Hello World!');
-  })
-app.get('/testwithheader', auth,  (req, res) => {
-  res.send('Hello World! with header');
 })
-app.post('/testwithheader', auth ,jsonParser,  (req, res) => {
+app.get('/testgetrequest', (req, res) => {
+	res.send('Hello World!');
+})
+app.get('/testwithheader', auth, (req, res) => {
+	res.send('Hello World! with header');
+})
+app.post('/testwithheader', auth, jsonParser, (req, res) => {
 	//console.log(req.body);
-	res.send('Hello World! with header n body'+req.body.name);
+	res.send('Hello World! with header n body' + req.body.name);
 })
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+	console.log(`Example app listening on port ${port}`)
 })
 
 //express started
