8b892931b0d08b96038c7a1ed1509b0a13db3867
Javed Khan (Node Dev) <javed.khan@technosoftwares.com>
2025-01-21 16:35:07 +0530

    dfdfdf

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,6 @@
 module.exports = {
     apps: [{
-        name: 'my-app',
-        script: 'server.js',
-        autorestart: true,
-        max_memory_restart: '1G',
-        env_development: {
-            "NODE_ENV": "development"
-        }
-    }],
-
-    deploy: {
-        development: {
-            user: 'ubuntu',
-            host: ['54.174.89.121'],
-            ref: 'origin/master',
-            repo: 'https://github.com/mohjavedali/testing-jekinsFile.git',
-            path: '/var/lib/jenkins/workspace/node-cicdjenkins',
-            'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js --env development'
-        }
-    }
-};
+        name: "app1",
+        script: "./server.js"
+    }]
+}
