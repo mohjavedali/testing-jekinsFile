680d4986e7af27b0c525cc4d10e459496646d00d
Javed Khan (Node Dev) <javed.khan@technosoftwares.com>
2025-01-21 17:07:03 +0530

    dfdfdfdf

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,16 @@
 module.exports = {
     apps: [
         {
-            name: 'my-app',
-            script: 'server.js', // Entry point of your app
-            env: {
+            name: 'my-app',                // Name of your application
+            script: './server.js',            // Entry point of your application
+            instances: 1,                  // Number of instances (use 'max' for all cores)
+            exec_mode: 'cluster',          // Cluster mode for multi-core usage
+            env: {                         // Default environment
+                NODE_ENV: 'production',
+            },
+            env_development: {             // Development environment
                 NODE_ENV: 'development',
+                DEBUG: 'my-app:*',           // Example environment variable
             },
         },
     ],
--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const auth = (req, res, next) => {
 	next();
 }
 app.get('/api', (req, res) => {
-	res.send('Hello World!!!!!!!what is dffdsafsfasfs');
+	res.send('Hello World!!!!!!!what is thissisisisi');
 })
 app.get('/api/testgetrequest', (req, res) => {
 	res.send('Hello World!');
