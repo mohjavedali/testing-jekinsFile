4a9127ec6dd59b31a7c5d833109a77b35a2e2338
Javed Khan (Node Dev) <javed.khan@technosoftwares.com>
2025-01-21 15:38:44 +0530

    sdfsddf

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const auth = (req, res, next) => {
 	next();
 }
 app.get('/api', (req, res) => {
-	res.send('Hello World!!!!!!!what is fafadsfadsfdsafadsdfasfadsfdsadf');
+	res.send('Hello World!!!!!!!what is dffdsa');
 })
 app.get('/api/testgetrequest', (req, res) => {
 	res.send('Hello World!');
